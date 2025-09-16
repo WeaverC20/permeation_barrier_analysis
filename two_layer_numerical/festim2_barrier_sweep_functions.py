58817a5d62fe34c91e46a13a3d0e7bf1bda07c61
WeaverC20 <67601091+WeaverC20@users.noreply.github.com>
2025-09-16 11:36:07 -0400

    found good equilibrium before doing sweep in two layer festim2 sim file. have t decide on good boundary values

--- two_layer_numerical/festim2_barrier_sweep_functions.py
+++ two_layer_numerical/festim2_barrier_sweep_functions.py
@@ -11,3 +11,26 @@ fieldnames = (
     + param_keys
 )
 
+def determine_regime(C1, Cm1, Cm2, C2, tolerance=0.05):
+    """
+    determines the regime based on concentration profile at steady state
+    if difference in concentration across layer less than "tolerance input" * C1, then it is surface limited
+    """
+    if abs(C1 - Cm1) / C1 < tolerance:
+        regime_barrier = "limited"
+    else:
+        regime_barrier = "limiting"
+
+    if abs(Cm2 - C2) / C1 < tolerance:
+        regime_substrate = "limited"
+    else:
+        regime_substrate = "limiting"
+
+    if regime_barrier == "limited" and regime_substrate == "limited":
+        regime_system = "surface-limited"
+    elif regime_barrier == "limiting" and regime_substrate == "limiting":
+        regime_system = "diffusion-limited"
+    else:
+        regime_system = "mixed"
+
+    return regime_barrier, regime_substrate, regime_system
