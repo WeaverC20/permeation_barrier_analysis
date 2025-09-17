aa4c3f22b39f77bf803c4d9a4e227c41b8ee4a82
WeaverC20 <67601091+WeaverC20@users.noreply.github.com>
2025-09-17 11:29:46 -0400

    function for generating sets of parameters and finished code to run set and output to csv

--- two_layer_numerical/festim2_barrier_sweep_functions.py
+++ two_layer_numerical/festim2_barrier_sweep_functions.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 param_keys = [
     "D_0_barrier", "E_D_barrier", "S_0_barrier", "E_S_barrier",
     "K_r_0_barrier", "E_K_r_barrier",
@@ -34,3 +36,36 @@ def determine_regime(C1, Cm1, Cm2, C2, tolerance=0.05):
         regime_system = "mixed"
 
     return regime_barrier, regime_substrate, regime_system
+
+def generate_params_list(base_params, S_0_limits, D_0_limits, n_points):
+    """
+    takes in the limits for S_0 and D_0 and generates a list of parameter sets
+    that spans the space of S_0 and D_0 values
+    uses a scale with more points in the middle of the range
+
+    outputs a list of dictionaries of size n_points^4
+    """
+    S_0_values = np.logspace(
+        np.log10(S_0_limits[0]),
+        np.log10(S_0_limits[1]),
+        n_points
+    )
+    D_0_values = np.logspace(
+        np.log10(D_0_limits[0]),
+        np.log10(D_0_limits[1]),
+        n_points
+    )
+
+    param_list = []
+    for S_0_barrier in S_0_values:
+        for S_0_substrate in S_0_values:
+            for D_0_barrier in D_0_values:
+                for D_0_substrate in D_0_values:
+                    params = base_params.copy()
+                    params["D_0_barrier"] = D_0_barrier
+                    params["S_0_barrier"] = S_0_barrier
+                    params["D_0_substrate"] = D_0_substrate
+                    params["S_0_substrate"] = S_0_substrate
+                    param_list.append(params)
+
+    return param_list
