b49f85142d25a29ef519019c4c65c7ba35b744d2
WeaverC20 <67601091+WeaverC20@users.noreply.github.com>
2026-01-15 11:23:10 -0500

    started analytical two layer regime analysis

---
+++ two_layer_analytical/2_layer_analytical.py
@@ -0,0 +1,8 @@
+surface_flux_coating = K_d*P_up - K_r*C_1**2
+flux_coating = -D_1*(C_1 - C_m1)/thickness_coating
+interface_eq = C_m2*K_s1 = C_m1*K_s2
+flux_substrate = -D_2*(C_m2 - C_2)/thickness_substrate
+surface_flux_substrate = K_r*C_2**2
+
+
+# after you do some analytical analysis
